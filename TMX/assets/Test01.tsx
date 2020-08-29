5d7edf98dfbca96e97f4e03aedba8306ad64843c
SpicyWinner <sonushivhare89@gmail.com>
2020-08-29 08:56:04 +0530

    +++
    To stop the View from going out of the map.

---
+++ TMX/assets/Test01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Test01" tilewidth="32" tileheight="8" tilecount="32400" columns="120">
+ <image source="Test01.png" width="3840" height="2160"/>
+</tileset>
--- TMX/main.cpp
+++ TMX/main.cpp
@@ -11,18 +11,20 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML window", sf::Style::Fullscreen);
     window.setFramerateLimit(200);
     tmx::Map map;
-    map.load("assets/kripper.tmx");
-    std::cout << std::endl;
+    map.load("assets/Test02.tmx");
 
     const auto& layers = map.getLayers();
-    const auto& collider = layers[2]->getLayerAs<tmx::ObjectGroup>();
+    const auto& collider = layers[1]->getLayerAs<tmx::ObjectGroup>();
 
     MapLayer backGround(map, 0);
-    MapLayer Objects(map, 1);
-    
+    // MapLayer Objects(map, 1);
+
+
+    // Remove
+    float delay = 0;
     sf::Font font;
     font.loadFromFile("C:/Windows/Fonts/arial.ttf");
     sf::Text text;
@@ -31,12 +33,12 @@ int main()
     text.setPosition(10.0f, 10.0f);
     text.setFillColor(sf::Color::White);
 
-    sf::CircleShape player(10.0f);
-    player.setOrigin(10.0f, 10.0f);
+    sf::RectangleShape player({ 64.0f, 160.0f });
+    player.setOrigin(32.0f, 64.0f);
     player.setPosition(10.0f, 500.0f);
 
-    sf::View gameView(player.getPosition(), sf::Vector2f(800.0f, 600.0f));
-    
+    sf::View gameView(player.getPosition(), sf::Vector2f(1920.0f, 1080.0f));
+
     std::vector<sf::FloatRect> tileMapColliders;
 
     for (auto& obj : collider.getObjects())
@@ -44,7 +46,7 @@ int main()
         tileMapColliders.push_back(sf::FloatRect(obj.getAABB().left, obj.getAABB().top, obj.getAABB().width, obj.getAABB().height));
     }
 
-    float playerSpeed = 1.0f;
+    float playerSpeed = 5.0f;
 
     sf::Clock globalClock, animationClock;
     while (window.isOpen())
@@ -77,17 +79,26 @@ int main()
         backGround.update(sf::Time(animationClock.getElapsedTime()));
 
         gameView.setCenter(player.getPosition());
+
+        if ((gameView.getCenter().x - gameView.getSize().x / 2) < 0) gameView.move(sf::Vector2f(gameView.getSize().x / 2 - player.getPosition().x, 0));
+        if ((gameView.getCenter().y - gameView.getSize().y / 2) < 0) gameView.move(sf::Vector2f(0, gameView.getSize().y / 2 - player.getPosition().y));
+        if ((gameView.getCenter().x + gameView.getSize().x / 2) > backGround.getGlobalBounds().width) gameView.move(sf::Vector2f(backGround.getGlobalBounds().width - (gameView.getCenter().x + gameView.getSize().x / 2), 0));
+        if ((gameView.getCenter().y + gameView.getSize().y / 2) > backGround.getGlobalBounds().height) gameView.move(sf::Vector2f(0, backGround.getGlobalBounds().height - (gameView.getCenter().y + gameView.getSize().y / 2)));
+
+
         window.setView(gameView);
 
-        text.setString(std::to_string(float(1.0f / delTime)));
-        text.setPosition(gameView.getCenter() - sf::Vector2f(700.0f, 600.0f));
-
-        std::string str = text.getString();
-        std::cout << str << std::endl;
+        // Remove
+        delay += delTime;
+        if (delay > 0.1)
+        {
+            text.setString(std::to_string(float(1.0f / delTime)));
+            delay = 0;
+        }
+        text.setPosition(gameView.getCenter().x - gameView.getSize().x / 2, gameView.getCenter().y - gameView.getSize().y / 2);
 
         window.clear(sf::Color::Black);
         window.draw(backGround);
-        window.draw(Objects);
         window.draw(player);
         window.draw(text);
         window.display();
